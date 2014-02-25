e1ce5aa84ee8a004cf407d9b176fbc41ada2f592
Mach <dev.winterfox@gmail.com>
2014-02-25 16:15:38 -0500

    New user setUp

--- Ribbit/src/main/java/com/arcticdev/ribbit/ribbit/RibbitApplication.java
+++ Ribbit/src/main/java/com/arcticdev/ribbit/ribbit/RibbitApplication.java
@@ -1,17 +1,14 @@
 package com.arcticdev.ribbit.ribbit;
 
 import android.app.Application;
+
 import com.parse.Parse;
-import com.parse.ParseObject;
 
 public class RibbitApplication extends Application {
 
     @Override
     public void onCreate() {
         Parse.initialize(this, "AYpvudiCdcDbYa9QYyliwzrq9f3pA34E7jNd5EoS", "WDNDkB9ax6TUG63ezzVw3kd9bbPfGzDzbvX75ZWg");
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
     }
 
 }
--- Ribbit/src/main/java/com/arcticdev/ribbit/ribbit/SignUpActivity.java
+++ Ribbit/src/main/java/com/arcticdev/ribbit/ribbit/SignUpActivity.java
@@ -1,16 +1,83 @@
 package com.arcticdev.ribbit.ribbit;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import java.text.ParseException;
 
 public class SignUpActivity extends Activity {
+
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected EditText mEmail;
+    protected Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mUsername = (EditText)findViewById(R.id.usernameField);
+        mPassword = (EditText)findViewById(R.id.passwordField);
+        mEmail = (EditText)findViewById(R.id.emailFeild);
+        mSignUpButton = (Button)findViewById(R.id.SignUpButton);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+                String email = mEmail.getText().toString();
+
+                username = username.trim();
+                password = password.trim();
+                email = email.trim();
+
+                if(username.isEmpty() || password.isEmpty() || email.isEmpty()){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.signUpErrorMessage)
+                            .setTitle(R.string.signUpErrorTitle)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }else{
+                    ParseUser newUser = new ParseUser();
+                    newUser.setUsername(username);
+                    newUser.setPassword(password);
+                    newUser.setEmail(email);
+                    newUser.signUpInBackground(new SignUpCallback(){
+                        @Override
+                        public void done(ParseException e){
+                            if(e == null){
+                                //Success!
+                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            }else{
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                                builder.setMessage(e.getMessage())
+                                        .setTitle(R.string.signUpErrorTitle)
+                                        .setPositiveButton(android.R.string.ok, null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
     }
 
 
